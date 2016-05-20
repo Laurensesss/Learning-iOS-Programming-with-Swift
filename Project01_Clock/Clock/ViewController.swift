f133b565ee7b2076f23d3b3b40d6b885032df00c
Laurensesss <veryverygood@qq.com>
2016-05-20 19:39:08 +0800

    Clock
    clear the comment.

--- Project01_Clock/Clock/ViewController.swift
+++ Project01_Clock/Clock/ViewController.swift
@@ -31,12 +31,10 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
 
