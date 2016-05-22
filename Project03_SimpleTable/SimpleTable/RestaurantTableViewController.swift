a8a085bb070df89813bfcf0748a6c2e4097e82f0
Laurensesss <veryverygood@qq.com>
2016-05-22 22:38:38 +0800

    project03

--- Project03_SimpleTable/SimpleTable/RestaurantTableViewController.swift
+++ Project03_SimpleTable/SimpleTable/RestaurantTableViewController.swift
@@ -14,18 +14,17 @@ class RestaurantTableViewController: UITableViewController {
   var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Thai Cafe"]
   var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "thaicafe.jpg"]
   var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-  
   var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+  var restaurantIsVisited = [Bool](count: 21, repeatedValue: false)
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     // Uncomment the following line to preserve selection between presentations
-//     self.clearsSelectionOnViewWillAppear = false
+    //     self.clearsSelectionOnViewWillAppear = false
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//     self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    //     self.navigationItem.rightBarButtonItem = self.editButtonItem()
   }
   
   override func didReceiveMemoryWarning() {
@@ -55,12 +54,57 @@ class RestaurantTableViewController: UITableViewController {
     cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
     cell.locationLabel.text = restaurantLocations[indexPath.row]
     cell.typeLabel.text = restaurantTypes[indexPath.row]
-//    cell.thumbnailImageView.layer.cornerRadius = 30
-//    cell.thumbnailImageView.clipsToBounds = true
+    
+    
+//        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.bounds.height / 2
+//        cell.thumbnailImageView.clipsToBounds = true
     cell.thumbnailImageView.layer.borderWidth = 3
     cell.thumbnailImageView.layer.borderColor = UIColor.orangeColor().CGColor
+    
+    cell.accessoryType = self.restaurantIsVisited[indexPath.row] ? .Checkmark : .None
     return cell
     
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    // Create an option menu as an action sheet.
+    let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+    
+    // Add actions to the menu.
+    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+    
+    let callActionHandler = { (action: UIAlertAction!) -> Void in
+      let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .Alert)
+      alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+      self.presentViewController(alertMessage, animated: true, completion: nil)
+      
+    }
+    
+    
+    let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .Default, handler: callActionHandler)
+    
+    
+    let isVisitedAction = UIAlertAction(title: "I've been here", style: .Default, handler: { (_) in
+      let cell = tableView.cellForRowAtIndexPath(indexPath)
+      cell?.accessoryType = .Checkmark
+      self.restaurantIsVisited[indexPath.row] = true
+    })
+    
+    
+    let isNotVisitedAction = UIAlertAction(title: "I've not been here", style: .Default) { (_) in
+      let cell = tableView.cellForRowAtIndexPath(indexPath)
+      cell?.accessoryType = .None
+      self.restaurantIsVisited[indexPath.row] = false
+    }
+    
+    let visitedAction = self.restaurantIsVisited[indexPath.row] ? isNotVisitedAction : isVisitedAction
+    
+    optionMenu.addAction(callAction)
+    optionMenu.addAction(visitedAction)
+    optionMenu.addAction(cancelAction)
+    self.presentViewController(optionMenu, animated: true, completion: nil)
+    
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
   
   
