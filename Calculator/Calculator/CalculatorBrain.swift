3fbe23088594af773bfd9f7a6fa3b2cd5f3ccd63
Laurensesss <veryverygood@qq.com>
2016-06-15 10:33:53 +0800

    Calculator

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -10,21 +10,24 @@ import Foundation
 
 class CalculatorBrain {
   
-  var description: String {
-    get {
-      return self.description
-    }
-    set {
-      self.description = String(newValue)
-    }
-  }
-  
-  var isPartialResult: Bool?
+//  var description: String {
+//    get {
+//      return self.description
+//    }
+//    set {
+//      self.description = String(newValue)
+//    }
+//  }
+//  
+//  var isPartialResult: Bool?
   
   private var accumulator = 0.0
   
+  private var internalProgram = [AnyObject]()
+  
   func setOperand(operand: Double) {
     accumulator = operand
+    internalProgram.append(operand)
   }
   
   private var operations: Dictionary<String,Operation> = [
@@ -50,6 +53,8 @@ class CalculatorBrain {
   }
   
   func performOperation(symbol: String) {
+    internalProgram.append(symbol)
+    
     if let operation = operations[symbol] {
       switch operation {
       case .Constant(let value):
@@ -77,6 +82,32 @@ class CalculatorBrain {
   private struct PendingBinaryOperationInfo {
     var bianryFunction: (Double ,Double) -> Double
     var firstOperand: Double
+  }
+  
+  typealias PropertyList = AnyObject
+  
+  var program: PropertyList {
+    get {
+      return internalProgram
+    }
+    set {
+      clear()
+      if let arrayOfOps = newValue as? [AnyObject] {
+        for op in arrayOfOps {
+          if let operand = op as? Double {
+            setOperand(operand)
+          } else if let operation = op as? String {
+            performOperation(operation)
+          }
+        }
+      }
+    }
+  }
+  
+  func clear() {
+    accumulator = 0
+    pending = nil
+    internalProgram.removeAll()
   }
  
   var result: Double {
